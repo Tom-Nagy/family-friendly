742a3847dd5b33222ac33d7869c4ddf6d2d73781
Tom-Nagy <tomnagy@hotmail.fr>
2021-07-04 12:10:26 +0000

    Add navbar dropdown functionality for small device

---
+++ app/static/js/scripts/main.js
@@ -0,0 +1,6 @@
+/* jshint esversion: 8 */
+
+// Js implementation of Materialize(https://materializecss.com/) functionality
+$(document).ready(function () {
+  $(".dropdown-button").dropdown();
+});
