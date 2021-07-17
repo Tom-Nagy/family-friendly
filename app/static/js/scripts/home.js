3d6906ac4e9890e655f0ed86a66d2cd4aff47d12
Tom-Nagy <tomnagy@hotmail.fr>
2021-07-17 14:04:51 +0000

    Add modals and hide links when user wants to create an event but is not logged in

--- app/static/js/scripts/home.js
+++ app/static/js/scripts/home.js
@@ -1,12 +1,21 @@
 /* jshint esversion: 8 */
 
 $(document).ready(function() {
+    // Trigger about modal
     $("#about-us-btn").on("click", function() {
         $("#about-modal").addClass("modal-active");
         $("#about-overlay").addClass("overlay-active");
     });
+
+    // Hide about modal
     $("#about-modal, #about-overlay").on("click", function() {
         $("#about-modal").removeClass("modal-active");
         $("#about-overlay").removeClass("overlay-active");
+    });
+
+    // Trigger try-create modal
+    $("#try-create").on("click", function() {
+        $("#try-create-modal").addClass("modal-active");
+        $("#home-overlay").addClass("overlay-active");
     });
 });
