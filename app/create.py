4f8cdde8f6947a71968f1e544216b8ab2381af0e
Tom-Nagy <tomnagy@hotmail.fr>
2021-07-19 09:41:44 +0000

    Add the create event form

--- app/create.py
+++ app/create.py
@@ -5,10 +5,18 @@ Views related to the creation of events
 from flask import (
     Flask, flash, render_template, redirect,
     request, session, url_for, Blueprint, current_app)
+from app.classes.user_class import User
 # from app.flashes.flash_messages import *
 
 create = Blueprint("create", __name__)
 
-@create.route("/create_event")
-def create_event():
-    return render_template('create_event.html')
+@create.route("/create_event/<username>", methods=["GET", "POST"])
+def create_event(username):
+
+    # Check if user is logged in and if session's user correspond to username
+    if session["user"]:
+        # Get user from the db and return a user collection
+        user = User.get_one_user_coll(username)
+        return render_template('create_event.html', user=user)
+    else:
+        return redirect(url_for('index.home'))
