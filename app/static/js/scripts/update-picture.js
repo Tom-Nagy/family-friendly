06a41e9e4622805eb3c1366e32bddf996f7796ec
Tom-Nagy <tomnagy@hotmail.fr>
2021-08-18 01:48:20 +0000

    Change the file size upload for the profile picture

--- app/static/js/scripts/update-picture.js
+++ app/static/js/scripts/update-picture.js
@@ -8,12 +8,10 @@ $(document).ready(function () {
         let size =
             (this.files[0].size / 1024 / 1024);
     
-        if (size > 60 || size < 0) {
-            alert("File size must be between 2-6 KB ");
+        if (size > 6 || size < 0.002) {
+            alert("File size must be between 1 KB and 6 MB ");
+            // empty the input field if the size is not correct
             $('#profile_picture').val("")
-        } else {
-            $("#output").html('<b>' +
-                'This file size is: ' + size + " MB" + '</b>');
         }
     });
 
